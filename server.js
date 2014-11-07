236563e5f9c3cc0c8a68a38c4b17de623c0f1d19
massimobiagioli <github@massimobiagioli.it>
2014-11-07 14:20:02 +0100

    aggiunte routes di insert e list

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
 #!/bin/env node
 
+'use strict';
+
 var express = require('express'),
 	http = require('http'),
-	mongoose = require('mongoose'),	
-	app = express(),	
+	cors = require('cors'),
+	mongoose = require('mongoose'),
+	app = express(),
 	nodeConfig = require('./lib/nodeConfig'),
 	mongoConfig = require('./lib/mongoConfig'),
-	routes = require('./lib/routes')(mongoose);
+	routes = require('./lib/routes')(mongoose),
+	db;
 
 // Configurazione app
 app.use(express.favicon());
+app.use(express.logger());
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(cors());
 app.use(app.router);
+app.use(express.static(__dirname + '/public'));
 
 // Routes
 app.get('/list', routes.list);
-app.get('/insert', routes.insert);
+app.post('/insert', routes.insert);
 
+// Connessione a MongoDb
 mongoose.connect(mongoConfig.getConnectionString());
-
-var db = mongoose.connection;
-
+db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', function() {
 	http.createServer(app).listen(nodeConfig.port, nodeConfig.host, function() {
