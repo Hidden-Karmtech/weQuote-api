470aba7e415360b98e9521383f2ccb0d7f3a87b8
massimobiagioli <github@massimobiagioli.it>
2015-01-31 16:27:59 +0100

    Aggiunto script per backup db

---
+++ .openshift/cron/daily/dumpdb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Backup MongoDB Database
+ 
+NOW="$(date +"%Y-%m-%d")"
+FILENAME="$OPENSHIFT_DATA_DIR/mongodb-dump.$NOW.tar.gz"
+
+mkdir -p /tmp/dump
+mongodump --host 127.7.119.130 --username admin --password a43H9aZb6iuE -o /tmp/dump
+
+tar -zcvf $FILENAME /tmp/dump
