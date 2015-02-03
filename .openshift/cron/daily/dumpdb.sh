5a351f7c9bbeb9794d035f2f58c19884a96e13de
massimobiagioli <github@massimobiagioli.it>
2015-02-03 13:52:56 +0100

    Modifica schedulazione db

--- .openshift/cron/daily/dumpdb.sh
+++ .openshift/cron/daily/dumpdb.sh
@@ -5,6 +5,6 @@ FILENAME="$OPENSHIFT_DATA_DIR/mongodb-dump-daily.tar.gz"
 TMPDIR="/tmp/dump"
 
 mkdir -p $TMPDIR
-mongodump --host 127.7.119.130 --username $OPENSHIFT_MONGODB_DB_USERNAME --password $OPENSHIFT_MONGODB_DB_PASSWORD -o $TMPDIR
+mongodump --host $OPENSHIFT_MONGODB_DB_HOST --username $OPENSHIFT_MONGODB_DB_USERNAME --password $OPENSHIFT_MONGODB_DB_PASSWORD -o $TMPDIR
 
 tar -zcvf $FILENAME $TMPDIR
